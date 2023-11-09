23bbf5214af5808304b954214794b9af4771ccfe
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2023-11-09 15:46:21 -0300

    Update sqlserver_assessment queries.py

--- queries.py
+++ queries.py
@@ -1,6 +1,14 @@
 queries = {
     "sqlserver_assessment": """
-      DECLARE @ts_now bigint = (SELECT cpu_ticks/(cpu_ticks/ms_ticks) FROM sys.dm_os_sys_info); 
+      DECLARE @totaliops DECIMAL;
+DECLARE @Throughput DECIMAL;
+
+
+SELECT 
+    @totaliops = ISNULL(SUM(io_stall / NULLIF((num_of_reads + num_of_writes), 0)), 0),
+    @Throughput = ISNULL(SUM((num_of_bytes_read + num_of_bytes_written) / 1048576), 0)
+FROM sys.dm_io_virtual_file_stats(NULL, NULL);
+
 
 SELECT 
     HostName,
@@ -11,8 +19,11 @@ SELECT
     TotalMemKB,
     AvaiMemKB,
     PLE,
+	@totaliops AS TotalIOPS,
+    @Throughput AS Throughput,
     DatabaseType AS [Database Type],
     [Event Time]
+  
 FROM (
     SELECT 
         HOST_NAME() AS HostName,
@@ -24,15 +35,15 @@ FROM (
         x.AvaiMem AS AvaiMemKB,
         x.PLE,
         @@Version AS DatabaseType,
-        DATEADD(ms, -1 * ((SELECT cpu_ticks/(cpu_ticks/ms_ticks) FROM sys.dm_os_sys_info) - [timestamp]), GETDATE()) AS [Event Time]
+        DATEADD(ms, -1 * ((SELECT cpu_ticks / (cpu_ticks / ms_ticks) FROM sys.dm_os_sys_info) - [timestamp]), GETDATE()) AS [Event Time]
     FROM (
         SELECT
             GETDATE() as collectionTime,
-            (si.committed_kb/1024) as Committed,
-            (si.committed_target_kb/1024) as targetCommited,
-            (sm.total_physical_memory_kb/1024) as totalMem,
-            (sm.available_physical_memory_kb/1024) as AvaiMem,
-            (SELECT SUM(cntr_value)/COUNT(*) FROM sys.dm_os_performance_counters 
+            (si.committed_kb / 1024) as Committed,
+            (si.committed_target_kb / 1024) as targetCommited,
+            (sm.total_physical_memory_kb / 1024) as totalMem,
+            (sm.available_physical_memory_kb / 1024) as AvaiMem,
+            (SELECT SUM(cntr_value) / COUNT(*) FROM sys.dm_os_performance_counters 
                 WHERE counter_name = 'Page Life expectancy' AND object_name LIKE '%buffer node%') as PLE
         FROM sys.dm_os_sys_info si, sys.dm_os_sys_memory sm
     ) AS x
