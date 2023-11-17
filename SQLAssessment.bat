42ddb25b7833af7d4c1d0dc78bdb869f37370c8b
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2023-11-17 18:16:49 -0300

    Update SQLAssessment.bat

--- SQLAssessment.bat
+++ SQLAssessment.bat
@@ -1,1 +1,3 @@
 "C:\Users\Administrator\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Administrator\Desktop\sql-report\py-monitor.py"
+
+"C:\Users\Administrator\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Administrator\Desktop\sql-report\95percentil.py"
