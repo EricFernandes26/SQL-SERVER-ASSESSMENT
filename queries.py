691d74c387c144cd33d2cd0bc51b10c978fb1ceb
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2023-11-21 18:06:44 -0300

    Update queries.py

--- queries.py
+++ queries.py
@@ -124,33 +124,7 @@ SELECT
 FROM #TempResults
 WHERE [Event Time] >= DATEADD(SECOND, -160, GETDATE())
 
-
-    """,
-
-    "sqlserver_assessment_Disk": """
-        SELECT
-            y.Montagem,
-            y.[Total (GB)],
-            y.[Espaço Disponível (GB)],
-            y.[Espaço Disponível (%)],
-            y.[Espaço em uso (%)]
-        FROM
-            (SELECT
-                MF.database_id,
-                MF.file_id,
-                VS.volume_mount_point AS [Montagem],
-                VS.logical_volume_name AS [Volume],
-                CAST(CAST(VS.total_bytes AS DECIMAL(19, 2)) / 1024 / 1024 / 1024 AS DECIMAL(10, 2)) AS [Total (GB)],
-                CAST(CAST(VS.available_bytes AS DECIMAL(19, 2)) / 1024 / 1024 / 1024 AS DECIMAL(10, 2)) AS [Espaço Disponível (GB)],
-                CAST((CAST(VS.available_bytes AS DECIMAL(19, 2)) / CAST(VS.total_bytes AS DECIMAL(19, 2)) * 100) AS DECIMAL(10, 2)) AS [Espaço Disponível (%)],
-                CAST((100 - CAST(VS.available_bytes AS DECIMAL(19, 2)) / CAST(VS.total_bytes AS DECIMAL(19, 2)) * 100) AS DECIMAL(10, 2)) AS [Espaço em uso (%)]
-            FROM
-                sys.master_files AS MF
-                CROSS APPLY [sys].[dm_os_volume_stats](MF.database_id, MF.file_id) AS VS
-            WHERE
-                CAST(VS.available_bytes AS DECIMAL(19, 2)) / CAST(VS.total_bytes AS DECIMAL(19, 2)) * 100 < 100
-            ) AS y;
-    """
+"""
 
     
 }
