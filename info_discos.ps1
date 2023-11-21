782b0456998463fb1bd8076de3ea9e9d595e31df
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2023-11-21 18:08:51 -0300

    file info_discos.ps1

---
+++ info_discos.ps1
@@ -0,0 +1,13 @@
+# Obter informações sobre discos lógicos
+$diskInfo = Get-WmiObject Win32_LogicalDisk | Select-Object DeviceID, VolumeName, `
+    @{Name="Size (GB)";Expression={"{0:N2}" -f ($_.Size / 1GB)}}, `
+    @{Name="FreeSpace (GB)";Expression={"{0:N2}" -f ($_.FreeSpace / 1GB)}}, `
+    @{Name="FreeSpacePercentage";Expression={"{0:N2}" -f ($_.FreeSpace / $_.Size * 100)}}
+
+# Caminho para o arquivo CSV
+$csvPath = "C:\Users\Administrator\Desktop\SQL-SERVER-ASSESSMENT\results\info_discos.csv"
+
+# Exportar para CSV com delimitador ;
+$diskInfo | Export-Csv -Path $csvPath -NoTypeInformation -Delimiter ';'
+
+Write-Host "As informacoees dos discos foram exportadas para $csvPath"
