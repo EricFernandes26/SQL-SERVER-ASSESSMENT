382a0777b1f33afb04dcc41049daaf9c555d6940
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2023-11-21 18:06:11 -0300

    Update SQLAssessment.bat

--- SQLAssessment.bat
+++ SQLAssessment.bat
@@ -1,3 +1,12 @@
-"C:\Users\Administrator\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Administrator\Desktop\sql-report\py-monitor.py"
+@echo off
 
-"C:\Users\Administrator\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Administrator\Desktop\sql-report\95percentil.py"
+rem Executa o script Python py-monitor.py
+"C:\Users\Administrator\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Administrator\Desktop\SQL-SERVER-ASSESSMENT\py-monitor.py"
+
+rem Executa o script Python 95percentil.py
+"C:\Users\Administrator\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Administrator\Desktop\SQL-SERVER-ASSESSMENT\95percentil.py"
+
+rem Executa o script PowerShell info_disco.ps1
+powershell.exe -File "C:\Users\Administrator\Desktop\SQL-SERVER-ASSESSMENT\info_discos.ps1"
+
+echo Scripts Python e PowerShell foram executados com sucesso!
