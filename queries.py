60a252b9d9596b8fece38def108029702ca42b70
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2023-11-27 16:48:21 -0300

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,27 +1,15 @@
 queries = {
     "sqlserver_assessment": """
--- Configurações iniciais
+--SAM-SQL07
 SET NOCOUNT ON;
-
--- Declaração de variáveis
-DECLARE 
-    @totaliops DECIMAL,
-    @Throughput DECIMAL,
-    @TotalMemKB DECIMAL,
-    @Percentil95_SQLProcessUtilization FLOAT,
-    @Percentil95_AvaiMemKB INT,
-    @Normalized_CPU FLOAT,
-    @Normalized_Mem FLOAT,
-    @CombinedScore FLOAT,
-    @Media_SQLProcessUtilization DECIMAL,
-    @Media_AvaiMemKB DECIMAL,
-    @InstanceType NVARCHAR(50);
+DECLARE @totaliops DECIMAL;
+DECLARE @Throughput DECIMAL;
+DECLARE @TotalMemKB DECIMAL;
 
 -- Parte 1: Obter total de memória física
 SELECT @TotalMemKB = (sm.total_physical_memory_kb / 1024)
 FROM sys.dm_os_sys_info si, sys.dm_os_sys_memory sm;
 
--- Calcular IOPS e Throughput
 SELECT 
     @totaliops = ISNULL(SUM(io_stall / NULLIF((num_of_reads + num_of_writes), 0)), 0),
     @Throughput = ISNULL(SUM((num_of_bytes_read + num_of_bytes_written) / 1048576), 0)
@@ -37,11 +25,16 @@ CREATE TABLE #TempResults (
     PLE DECIMAL,
     Percentil95_SQLProcessUtilization DECIMAL,
     Percentil95_AvaiMemKB DECIMAL,
+    VCpu INT,
+    Memory INT,
+    Throughput INT,
+    VCPUUtilization INT,
+    MemoryUtilization INT,
     [Event Time] DATETIME
 );
 
 -- Calcular valores intermediários e inserir na tabela temporária
-INSERT INTO #TempResults
+INSERT INTO #TempResults (HostName, NumberOfCPUs, SQLProcessUtilization, TotalMemKB, AvaiMemKB, PLE, Percentil95_SQLProcessUtilization, Percentil95_AvaiMemKB, [Event Time])
 SELECT 
     HOST_NAME() AS HostName,
     (SELECT cpu_count FROM sys.dm_os_sys_info) AS NumberOfCPUs,
@@ -77,36 +70,61 @@ CROSS APPLY (
 ) AS y;
 
 -- Calcular a média da coluna SQLProcessUtilization
+DECLARE @Media_SQLProcessUtilization DECIMAL;
 SELECT @Media_SQLProcessUtilization = AVG(SQLProcessUtilization) FROM #TempResults;
 
 -- Calcular a média da coluna AvaiMemKB
+DECLARE @Media_AvaiMemKB DECIMAL;
 SELECT @Media_AvaiMemKB = AVG(AvaiMemKB) FROM #TempResults;
 
--- Normalizar as métricas para ficarem entre 0 e 100
-SET @Normalized_CPU = (@Percentil95_SQLProcessUtilization / 100.0) * 50.0; -- Peso 50%
-SET @Normalized_Mem = (@Percentil95_AvaiMemKB / 102400.0) * 50.0; -- Peso 50%
+-- Adicionar a lógica para definir @InstanceType
+DECLARE @InstanceType NVARCHAR(50);
 
--- Calcular a pontuação combinada (pesos ajustáveis conforme necessário)
-SET @CombinedScore = @Normalized_CPU + @Normalized_Mem;
+-- Lógica para sugerir valores
+DECLARE @SuggestedVCPU INT,
+        @SuggestedMemory INT,
+        @SuggestedThroughput INT,
+        @SuggestedVCPUUtilization INT,
+        @SuggestedMemoryUtilization INT;
 
--- Definir @InstanceType com base na pontuação combinada
-SET @InstanceType = 
-    CASE
-        WHEN @CombinedScore BETWEEN 0 AND 10 THEN 't3.nano'
-        WHEN @CombinedScore BETWEEN 11 AND 19 THEN 't3.medium'
-        WHEN @CombinedScore BETWEEN 20 AND 29 THEN 't3.large'
-        WHEN @CombinedScore BETWEEN 30 AND 39 THEN 'm5.large'
-        WHEN @CombinedScore BETWEEN 40 AND 49 THEN 'm5.xlarge'
-        WHEN @CombinedScore BETWEEN 50 AND 59 THEN 'm5.2xlarge'
-        WHEN @CombinedScore BETWEEN 60 AND 69 THEN 'm5.2xlarge'
-        WHEN @CombinedScore BETWEEN 70 AND 79 THEN 'r6i.8xlarge'
-        WHEN @CombinedScore BETWEEN 80 AND 89 THEN 'r6i.12xlarge'
-        WHEN @CombinedScore BETWEEN 90 AND 99 THEN 'r6i.16xlarge'
-        WHEN @CombinedScore >= 100 THEN 'x2iedn.32xlarge'
-        ELSE 't3.small' -- Defina o tipo de instância padrão
-    END;
+-- Lógica para sugerir valores de forma aleatória
+SET @SuggestedVCPU = CAST(RAND() * (32 - 1) + 1 AS INT);  -- Intervalo de 1 a 32 VCPUs
+SET @SuggestedMemory = CAST(RAND() * (65536 - 1024) + 1024 AS INT);  -- Intervalo de 1024 MB a 65,536 MB (64 GB)
+SET @SuggestedThroughput = CAST(RAND() * (1000 - 1) + 1 AS INT);  -- Intervalo de 1 a 1000
+SET @SuggestedVCPUUtilization = CAST(RAND() * (100 - 1) + 1 AS INT);  -- Intervalo de 1 a 100
+SET @SuggestedMemoryUtilization = CAST(RAND() * (100 - 1) + 1 AS INT);  -- Intervalo de 1 a 100
 
--- Selecionar resultados finais da tabela temporária
+-- Inserir os valores sugeridos na tabela temporária
+UPDATE #TempResults
+SET
+    VCpu = @SuggestedVCPU,
+    Memory = @SuggestedMemory,
+    Throughput = @SuggestedThroughput,
+    VCPUUtilization = @SuggestedVCPUUtilization,
+    MemoryUtilization = @SuggestedMemoryUtilization;
+
+-- Lógica para determinar a instância EC2
+SELECT TOP 1
+    @InstanceType = InstanceType
+FROM
+    (SELECT
+        CASE
+			WHEN @SuggestedVCPUUtilization >= 0 AND @SuggestedMemoryUtilization <=  10 THEN 't3.nano'
+			WHEN @SuggestedVCPUUtilization >= 11 AND @SuggestedMemoryUtilization <= 19 THEN 't3.medium'
+            WHEN @SuggestedVCPUUtilization >= 20 AND @SuggestedMemoryUtilization <= 29 THEN 't3.large'
+            WHEN @SuggestedVCPUUtilization >= 30 AND @SuggestedMemoryUtilization <= 39 THEN 'm5.large'
+			WHEN @SuggestedVCPUUtilization >= 40 AND @SuggestedMemoryUtilization <= 49 THEN 'm5.xlarge'
+			WHEN @SuggestedVCPUUtilization >= 50 AND @SuggestedMemoryUtilization <= 59 THEN 'm5.2xlarge'
+			WHEN @SuggestedVCPUUtilization >= 60 AND @SuggestedMemoryUtilization <= 69 THEN 'r6i.8xlarge'
+			WHEN @SuggestedVCPUUtilization >= 70 AND @SuggestedMemoryUtilization <= 79 THEN 'r6i.12xlarge'
+            WHEN @SuggestedVCPUUtilization >= 80 AND @SuggestedMemoryUtilization <= 89 THEN 'r6i.16xlarge'
+			WHEN @SuggestedVCPUUtilization >= 90 AND @SuggestedMemoryUtilization <= 99 THEN 'x2iedn.32xlarge'
+            ELSE 't3.micro'
+        END AS InstanceType
+    FROM
+        #TempResults) AS SubQuery;
+
+-- Consulta para obter resultados finais
 SELECT 
     HostName,
     NumberOfCPUs AS NumberOfCPU,
@@ -117,16 +135,17 @@ SELECT
     @totaliops AS TotalIOPS,
     @Throughput AS Throughput,
     Percentil95_SQLProcessUtilization,
-    @Media_SQLProcessUtilization AS Media_SQLProcessUtilization,  
+    @Media_SQLProcessUtilization AS Media_SQLProcessUtilization,
     MAX(Percentil95_AvaiMemKB) OVER (PARTITION BY HostName) AS Percentil95_AvaiMemKB,
     AVG(AvaiMemKB) OVER (PARTITION BY HostName) AS Media_AvaiMemKB,
-    @InstanceType AS InstanceType, 
+    @InstanceType AS InstanceType,
     [Event Time]
 FROM #TempResults
 WHERE [Event Time] >= DATEADD(MINUTE, -10, GETDATE());
 
--- Drop a tabela temporária
+-- Drop the temporary table
 DROP TABLE #TempResults;
+
 
 
 """
