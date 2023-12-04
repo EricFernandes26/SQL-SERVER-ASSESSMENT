827af0839678a7aa578d9feaca638293c2496e87
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2023-12-04 12:15:55 -0300

    Update info_discos.ps1

--- info_discos.ps1
+++ info_discos.ps1
@@ -1,3 +1,7 @@
+# Obter informações sobre a CPU
+$cpuInfo = Get-WmiObject Win32_Processor
+$cpuName = $cpuInfo.Name
+
 # Obter informações sobre discos lógicos
 $diskInfo = Get-WmiObject Win32_LogicalDisk | Select-Object DeviceID, VolumeName, `
     @{Name="Size (GB)";Expression={"{0:N2}" -f ($_.Size / 1GB)}}, `
@@ -7,6 +11,15 @@ $diskInfo = Get-WmiObject Win32_LogicalDisk | Select-Object DeviceID, VolumeName
 # Caminho para o arquivo CSV
 $csvPath = "C:\Users\Administrator\Desktop\SQL-SERVER-ASSESSMENT\results\info_discos.csv"
 
-# Exportar para CSV com delimitador ;
-$diskInfo | Export-Csv -Path $csvPath -NoTypeInformation -Delimiter ';'
+# Criar um objeto contendo as informações da CPU e dos discos
+$result = New-Object PSObject -Property @{
+    'CPU Name' = $cpuName
+    'DeviceID' = $diskInfo.DeviceID
+    'VolumeName' = $diskInfo.VolumeName
+    'Size (GB)' = $diskInfo.'Size (GB)'
+    'FreeSpace (GB)' = $diskInfo.'FreeSpace (GB)'
+    'FreeSpacePercentage' = $diskInfo.FreeSpacePercentage
+}
 
+# Exportar para CSV com delimitador ;
+$result | Export-Csv -Path $csvPath -NoTypeInformation -Delimiter ';'
