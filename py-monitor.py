f111e620c5069c87d88a8a8c0d7e63b6f38ec829
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2023-12-13 18:40:24 -0300

    Update py-monitor.py

--- py-monitor.py
+++ py-monitor.py
@@ -7,36 +7,32 @@ import getpass
 def get_connection_info():
     print("Please enter your database connection information:")
     user = input("Username: ")
-    password = getpass.getpass("Password: ")  # Isso ocultará a senha inserida
+    password = getpass.getpass("Password: ")
     dbserver = input("Server: ")
     dbname = input("Database name: ")
     return user, password, dbserver, dbname
 
 def sqlserver_assessment(csv_file_name, user, password, dbserver, dbname, query):
-    # Estabelece a conexão
     if user is not None and password is not None:
         connection = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={dbserver};DATABASE={dbname};UID={user};PWD={password}')
     else:
         connection = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={dbserver};DATABASE={dbname}')
 
-    # Executa a query.
     cursor = connection.execute(query)
 
-    # Obtém os resultados da query.
-    results = []
-    for row in cursor:
-        results.append(row)
+    # Obter todas as linhas de uma vez
+    results = cursor.fetchall()
 
-    # Cria um arquivo CSV e escreve os resultados nele.
     with open(f"./results/{csv_file_name}", 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile, delimiter=';')
+        
         # Escreve a linha de cabeçalho
         header = [column[0] for column in cursor.description]
         csv_writer.writerow(header)
+
         # Escreve as linhas de dados
         csv_writer.writerows(results)
 
-    # Fecha o cursor e a conexão.
     cursor.close()
     connection.close()
 
