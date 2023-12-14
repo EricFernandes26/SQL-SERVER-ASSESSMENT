d67aa408a864e676f64fd43c847a7caa07347135
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2023-12-14 16:08:53 -0300

    Update py-monitor.py

--- py-monitor.py
+++ py-monitor.py
@@ -1,42 +1,43 @@
 import pyodbc
 import csv
 import os
+import pandas as pd
 from queries import queries
+from sqlalchemy import create_engine, text
 import getpass
 
 def get_connection_info():
-    print("Please enter your database connection information:")
+    print("Por favor, insira as informações de conexão com o banco de dados:")
     user = input("Username: ")
     password = getpass.getpass("Password: ")
-    dbserver = input("Server: ")
-    dbname = input("Database name: ")
+    dbserver = input("Server-name: ")
+    dbname = input("Database-name: ")
     return user, password, dbserver, dbname
 
-def sqlserver_assessment(csv_file_name, user, password, dbserver, dbname, query):
-    if user is not None and password is not None:
-        connection = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={dbserver};DATABASE={dbname};UID={user};PWD={password}')
+def sqlserver_assessment_para_dataframe(user, password, dbserver, dbname, query):
+    connection_str = f'mssql+pyodbc://{user}:{password}@{dbserver}/{dbname}?driver=ODBC+Driver+17+for+SQL+Server'
+    engine = create_engine(connection_str)
+
+    # Use a conexão do SQLAlchemy para executar a consulta
+    connection = engine.connect()
+
+    # Converta a string da consulta em um objeto TextClause
+    query_obj = text(query)
+
+    # Execute a consulta
+    result = connection.execute(query_obj)
+
+    # Verifique se há algum resultado antes de tentar obter as linhas
+    if result.returns_rows:
+        # Use o pandas para ler os resultados em um DataFrame
+        df = pd.DataFrame(result.fetchall(), columns=result.keys())
     else:
-        connection = pyodbc.connect(f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={dbserver};DATABASE={dbname}')
+        df = pd.DataFrame()
 
-    cursor = connection.execute(query)
-
-    # Obter todas as linhas de uma vez
-    results = cursor.fetchall()
-
-    with open(f"./results/{csv_file_name}", 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=';')
-        
-        # Escreve a linha de cabeçalho
-        header = [column[0] for column in cursor.description]
-        csv_writer.writerow(header)
-
-        # Escreve as linhas de dados
-        csv_writer.writerows(results)
-
-    cursor.close()
+    # Feche a conexão após obter os resultados
     connection.close()
 
-    return results
+    return df
 
 if __name__ == "__main__":
     if not os.path.exists('results'):
@@ -44,5 +45,17 @@ if __name__ == "__main__":
 
     user, password, dbserver, dbname = get_connection_info()
 
+    # Cria um escritor Pandas Excel usando o XlsxWriter como mecanismo.
+    excel_writer = pd.ExcelWriter('./results/resultados_consultas.xlsx', engine='xlsxwriter')
+
     for query_key, query_value in queries.items():
-        sqlserver_assessment(csv_file_name=f"{query_key}.csv", query=query_value, user=user, password=password, dbserver=dbserver, dbname=dbname)
+        # Ajuste os nomes das abas para garantir que sejam <= 31 caracteres
+        sheet_name = query_key[:31]
+        
+        df = sqlserver_assessment_para_dataframe(user, password, dbserver, dbname, query_value)
+
+        # Escreve o DataFrame no arquivo Excel
+        df.to_excel(excel_writer, sheet_name=sheet_name, index=False)
+
+    # Fecha o escritor Pandas Excel (não é necessário chamar um método save() aqui)
+    excel_writer.close()
